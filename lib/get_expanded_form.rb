2f7ae2096b6bc4cedecbf1afa29a110486477a54
archiemartini <archie.cgm@gmail.com>
2022-11-08 16:46:31 +0000

    2 and 3 form number passing

--- lib/get_expanded_form.rb
+++ lib/get_expanded_form.rb
@@ -1,3 +1,14 @@
 def get_expanded_form(num)
-  num
+
+  num_strings = num.to_s.split('')
+  if num_strings.length == 2
+    num_strings[0] = num_strings[0] + "0"
+    return num_strings.join(" + ")
+  elsif num_strings.length == 3
+    num_strings[0] = num_strings[0] + "00"
+    num_strings[1] = num_strings[1] + "0"
+    return num_strings.join(" + ")
+  else 
+    return num
+  end
 end
--- spec/get_extended_number_spec.rb
+++ spec/get_extended_number_spec.rb
@@ -6,4 +6,15 @@ RSpec.describe "get_expanded_form method" do
     expect(get_expanded_form(1)).to eq 1
   end
 
+  it "returns a 2 form number correctly" do
+    expect(get_expanded_form(11)).to eq '10 + 1'
+  end
+  it "returns a 2 form number correctly" do
+    expect(get_expanded_form(78)).to eq '70 + 8'
+  end
+
+  it "returns a 3 form number correctly" do
+    expect(get_expanded_form(111)).to eq '100 + 10 + 1'
+  end
+
 end
