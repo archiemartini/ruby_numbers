70962e457cc12bea2d1d96ebab3d961ac2eb3698
archiemartini <archie.cgm@gmail.com>
2022-11-08 17:18:03 +0000

    refactored logic into zero_expansion_helper

--- lib/get_expanded_form.rb
+++ lib/get_expanded_form.rb
@@ -1,19 +1,39 @@
 def get_expanded_form(num)
   num_strings = num.to_s.split('')
+  digit_length = num_strings.length
 
-  if num_strings.length == 2
+  if digit_length == 2
     num_strings[0] = num_strings[0] + "0"
     return num_strings.join(" + ")
-  elsif num_strings.length == 3
+  elsif digit_length == 3
     num_strings[0] = num_strings[0] + "00"
     num_strings[1] = num_strings[1] + "0"
     return num_strings.join(" + ")
-  elsif num_strings.length == 4
+  elsif digit_length == 4
     num_strings[0] = num_strings[0] + "000"
     num_strings[1] = num_strings[1] + "00"
     num_strings[2] = num_strings[2] + "0"
     return num_strings.join(" + ")
+  elsif digit_length == 5
+    return zero_expansion_helper(num_strings, digit_length)
   else 
     return num
   end
+
 end
+
+def zero_expansion_helper(num_strings, digit_length)
+  nums = num_strings
+  n = digit_length - 1
+  for i in 0..n do
+    nums[i] = nums[i] + ("0" * (digit_length - i - 1))
+  end
+  return nums.join(" + ")
+end
+
+# i = 0
+# while i < digit_length do
+#   num_strings[i] = num_strings[i] + ("0" * (digit_length - i - 1))
+#   i++
+# end
+# return num_strings
--- spec/get_extended_number_spec.rb
+++ spec/get_extended_number_spec.rb
@@ -20,4 +20,8 @@ RSpec.describe "get_expanded_form method" do
   it "returns a 4 form number correctly" do
     expect(get_expanded_form(1111)).to eq '1000 + 100 + 10 + 1'
   end
+  
+  it "returns a 5 form number correctly" do
+    expect(get_expanded_form(11111)).to eq '10000 + 1000 + 100 + 10 + 1'
+  end
 end
