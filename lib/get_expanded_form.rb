63757c78523874646e22731bf2d8c4b7d0a46d8d
archiemartini <archie.cgm@gmail.com>
2022-11-08 17:37:16 +0000

    refactored zer_expansion_helper into getexpanded_form

--- lib/get_expanded_form.rb
+++ lib/get_expanded_form.rb
@@ -1,39 +1,16 @@
 def get_expanded_form(num)
-  num_strings = num.to_s.split('')
-  digit_length = num_strings.length
+  nums = num.to_s.split('')
+  digit_length = nums.length
 
-  if digit_length == 2
-    num_strings[0] = num_strings[0] + "0"
-    return num_strings.join(" + ")
-  elsif digit_length == 3
-    num_strings[0] = num_strings[0] + "00"
-    num_strings[1] = num_strings[1] + "0"
-    return num_strings.join(" + ")
-  elsif digit_length == 4
-    num_strings[0] = num_strings[0] + "000"
-    num_strings[1] = num_strings[1] + "00"
-    num_strings[2] = num_strings[2] + "0"
-    return num_strings.join(" + ")
-  elsif digit_length == 5
-    return zero_expansion_helper(num_strings, digit_length)
-  else 
-    return num
-  end
-
-end
-
-def zero_expansion_helper(num_strings, digit_length)
-  nums = num_strings
   n = digit_length - 1
   for i in 0..n do
-    nums[i] = nums[i] + ("0" * (digit_length - i - 1))
+    if nums[i] == "0"
+      nums[i] = ""
+    else
+     nums[i] = nums[i] + ("0" * (digit_length - i - 1))
+    end
   end
-  return nums.join(" + ")
+  
+  return nums.reject(&:empty?).join(" + ")
 end
 
-# i = 0
-# while i < digit_length do
-#   num_strings[i] = num_strings[i] + ("0" * (digit_length - i - 1))
-#   i++
-# end
-# return num_strings
--- spec/get_extended_number_spec.rb
+++ spec/get_extended_number_spec.rb
@@ -3,7 +3,7 @@ require 'get_expanded_form'
 RSpec.describe "get_expanded_form method" do
   
   it "returns a single number correctly" do
-    expect(get_expanded_form(1)).to eq 1
+    expect(get_expanded_form(1)).to eq "1"
   end
 
   it "returns a 2 form number correctly" do
@@ -20,8 +20,12 @@ RSpec.describe "get_expanded_form method" do
   it "returns a 4 form number correctly" do
     expect(get_expanded_form(1111)).to eq '1000 + 100 + 10 + 1'
   end
-  
+
   it "returns a 5 form number correctly" do
     expect(get_expanded_form(11111)).to eq '10000 + 1000 + 100 + 10 + 1'
+  end
+
+  it "returns a fucker correctly" do
+    expect(get_expanded_form(70304)).to eq "70000 + 300 + 4"
   end
 end
